e65a6241f2e19b129346393f6cc0487f4c659f33
SilverHof <63652935+SilverHof@users.noreply.github.com>
2022-06-30 15:22:19 +0300

    add function firebaseSetup() to appMain.js and add get data in firebase.js

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -2,11 +2,13 @@ import '../scss/main.scss';
 import '../html/index.html';
 import { changeLocation } from './changeLocation.js';
 import { instructionForm } from './instructionForm.js';
+import { firebaseSetup } from './firebase';
 
 
 const appMain = () => {
     changeLocation();
     instructionForm();
+    firebaseSetup();
 }
 
 
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,17 +1,47 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { 
+    getFirestore,
+    collection,
+    getDocs 
+} from "firebase/firestore";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAAo9QTQsAgicQxE3v98Vsv5DRDTboiFTI",
-  authDomain: "tele2-practice.firebaseapp.com",
-  projectId: "tele2-practice",
-  storageBucket: "tele2-practice.appspot.com",
-  messagingSenderId: "995871636480",
-  appId: "1:995871636480:web:b2e23a9c1364ce33ceace5"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const firebaseSetup = () => {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyAAo9QTQsAgicQxE3v98Vsv5DRDTboiFTI",
+      authDomain: "tele2-practice.firebaseapp.com",
+      projectId: "tele2-practice",
+      storageBucket: "tele2-practice.appspot.com",
+      messagingSenderId: "995871636480",
+      appId: "1:995871636480:web:b2e23a9c1364ce33ceace5"
+    };
+    
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    
+    // init service
+    const dataBase = getFirestore();
+    
+    // collection reference
+    const collectionRef = collection(dataBase, 'countries');
+    
+    // get collection data
+    getDocs(collectionRef)
+        .then((snapshot) => {
+            let countries = [];
+            snapshot.docs.forEach((doc) => {
+                countries.push({
+                    ...doc.data(),
+                    id: doc.id
+                })
+            });
+            console.log(countries);
+            console.log(countries[2].name);
+        })
+        .catch(error => {
+            console.log(error.message);        
+        })
+}
