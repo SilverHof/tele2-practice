3fbd0e459b1869a65f0e624cd09c8d20360b83f1
SilverHof <63652935+SilverHof@users.noreply.github.com>
2022-06-30 19:40:50 +0300

    change file names and functions names

--- src/js/appMain.js
+++ src/js/appMain.js
@@ -2,11 +2,11 @@ import '../scss/main.scss';
 import '../html/index.html';
 import { changeLocation } from './changeLocation.js';
 import { instructionForm } from './instructionForm.js';
-import { firebaseSetup } from './firebase';
+import { cityFirebaseSetup } from './cityFirebaseSetup.js';
 
 
 const appMain = () => {
-    firebaseSetup();
+    cityFirebaseSetup();
     changeLocation();
     instructionForm();
 }
