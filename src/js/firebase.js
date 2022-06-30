ad181b8fc9ba48346dc721da789c132bfd6d6207
SilverHof <63652935+SilverHof@users.noreply.github.com>
2022-06-30 13:58:15 +0300

    add configuration firebase.js

---
+++ src/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAo9QTQsAgicQxE3v98Vsv5DRDTboiFTI",
+  authDomain: "tele2-practice.firebaseapp.com",
+  projectId: "tele2-practice",
+  storageBucket: "tele2-practice.appspot.com",
+  messagingSenderId: "995871636480",
+  appId: "1:995871636480:web:b2e23a9c1364ce33ceace5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
